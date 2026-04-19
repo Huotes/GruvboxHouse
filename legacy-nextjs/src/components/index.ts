b17544d6cadce7865249602c783c648b3750d74e
Athos Windows 10 <aureliodosol@gmail.com>
2026-04-19 20:12:18 -0300

    chore: remove legacy next

--- legacy-nextjs/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { Header } from "./Header";
-export { Hero } from "./Hero";
-export { Services } from "./Services";
-export { HowItWorks } from "./HowItWorks";
-export { Technologies } from "./Technologies";
-export { CTA } from "./CTA";
-export { Contact } from "./Contact";
-export { Footer } from "./Footer";
-export { Planet, Spaceship, FloatingAlien, Nebula, OrbitDot } from "./SpaceDecorations";
--- legacy-nextjs/src/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const WA_NUM = "5518997896156";
-export const WA_LINK = `https://wa.me/${WA_NUM}?text=${encodeURIComponent("Olá! Vim pelo site da Gruvbox House e gostaria de um orçamento.")}`;
-export const WA_DISPLAY = "+55 (18) 99789-6156";
-export const EMAIL = "gruvboxhouse@gmail.com";
